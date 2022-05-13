4d44e55903d1530a53b9c25ad6501ab63a6358cb
bmd-rezafahlevi <reza.fahlevi@bhinneka.com>
2022-05-13 22:14:41 +0700

    adding gorm

---
+++ app/app/index_handler.go
@@ -0,0 +1,9 @@
+package app
+
+// func HandleIndex(w http.ResponseWriter, _ *http.Request) {
+// 	w.Header().Set("Content-Length", "12")
+// 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+// 	w.Header().Set("X-Content-Type-Options", "nonsif")
+
+// 	w.Write([]byte("Hello World!"))
+// }
